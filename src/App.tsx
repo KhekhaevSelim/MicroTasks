2b3afdb0ab53ef56cefb3056134175ee9e3f3f7d
KhekhaevSelim <selim16695@mail.ru>
2023-01-05 23:12:28 +0300

    firstHomeWorkCommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type CurrentButtonType = "All" | 'Rubles' | 'Dollars'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'Rubles', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'Rubles', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'Rubles', value: 50, number: ' v1234567890'},
+    ])
+    let currentMoney = money;
+
+    let [currentButton, setNameButton] = useState<CurrentButtonType>("All")
+
+    if(currentButton === 'Rubles') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Rubles')
+    }
+    if (currentButton === 'Dollars') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars')
+
+    }
+    const onclickFilterHandler = (nameButton: CurrentButtonType) => {
+        setNameButton(nameButton)
+
+
+    }
+
+    return (
+        <>
+            <ul>
+                {currentMoney.map((objectFromMoneyArr, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{objectFromMoneyArr.banknots}</span>
+                            <span> {objectFromMoneyArr.value}</span>
+                            <span>{objectFromMoneyArr.number}</span>
+                        </li>
+
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft: "30px"}}>
+                <button onClick={() => onclickFilterHandler("All")}>All</button>
+                <button onClick={() => onclickFilterHandler("Rubles")}>Rubles</button>
+                <button onClick={() => onclickFilterHandler("Dollars")}>Dollars</button>
+            </div>
+
+        </>
+
+
+    )
+
+
 }
 
 export default App;
